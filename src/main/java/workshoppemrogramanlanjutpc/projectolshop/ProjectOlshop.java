d55fe870caf713cfb2f4a4d58c29859c6bf42c21
mahyaSAJA <raffim856@gmail.com>
2024-11-26 06:40:18 +0700

    dari mahya. tambahan config database

--- src/main/java/workshoppemrogramanlanjutpc/projectolshop/ProjectOlshop.java
+++ src/main/java/workshoppemrogramanlanjutpc/projectolshop/ProjectOlshop.java
@@ -8,10 +8,41 @@ package workshoppemrogramanlanjutpc.projectolshop;
  *
  * @author BalbalPC
  */
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ProjectOlshop {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Connection conn = dbconf.connect(); // Panggil koneksi database
+        if (conn != null) {
+            try {
+                String query = "SELECT * FROM produk"; // Ganti dengan nama tabel Anda
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+
+                // Loop melalui hasil query
+                while (rs.next()) {
+                    // Contoh: Ambil data dari kolom
+                    int id = rs.getInt("prod_id"); // Ganti "id" dengan nama kolom Anda
+                    String name = rs.getString("nama"); // Ganti "name" dengan nama kolom Anda
+                    String harga = rs.getString("harga");
+                    String kategori = rs.getString("kategori");
+                    String deskripsi = rs.getString("deskripsi");
+                    System.out.println("ID: " + id + ", Name: " + name + "harga: "+harga);
+                }
+
+                // Tutup koneksi
+                rs.close();
+                stmt.close();
+                conn.close();
+            } catch (Exception e) {
+                System.out.println("Error saat query data: " + e.getMessage());
+            }
+        }
     }
 }
 //Token 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
---
+++ src/main/java/workshoppemrogramanlanjutpc/projectolshop/dbconf.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package workshoppemrogramanlanjutpc.projectolshop;
+
+/**
+ *
+ * @author W I N D O W S
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbconf {
+     public static Connection connect() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:mysql://127.0.0.1:3306/projekwpl";; // Ganti dengan nama database Anda
+            String user = "root"; // Ganti dengan username Anda
+            String password = ""; // Ganti dengan password Anda (kosongkan jika tidak ada password)
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Koneksi berhasil!");
+        } catch (SQLException e) {
+            System.out.println("Koneksi gagal: " + e.getMessage());
+        }
+        return conn;
+    }
+}
