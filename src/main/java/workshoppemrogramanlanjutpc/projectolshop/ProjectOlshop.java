529746382e887c5c10fc2c0592721e27d7922310
BalbalPC <BalbalPC@DESKTOP-HF89SUR>
2024-11-25 19:42:45 +0700

    New

---
+++ src/main/java/workshoppemrogramanlanjutpc/projectolshop/ProjectOlshop.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package workshoppemrogramanlanjutpc.projectolshop;
+
+/**
+ *
+ * @author BalbalPC
+ */
+public class ProjectOlshop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+//Token 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
